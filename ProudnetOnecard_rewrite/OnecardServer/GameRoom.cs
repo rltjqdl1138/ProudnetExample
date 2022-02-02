56f191fd062348226805546800cdcd96d234edb1
Kim ki seop <rltjqdl1138@naver.com>
2022-02-02 19:03:52 +0900

    temp

--- ProudnetOnecard_rewrite/OnecardServer/GameRoom.cs
+++ ProudnetOnecard_rewrite/OnecardServer/GameRoom.cs
@@ -14,8 +14,18 @@ namespace OnecardServer
 		public Stack<GameCard> unusedDeck = new Stack<GameCard>();
 		public GameCard lastCard = new GameCard();
 
+		public bool EnterPlayer(User user)
+		{
+			int numPlayer = players.Count();
+			if (numPlayer >= 4) return false;
+
+			GamePlayer newPlayer = new GamePlayer(user);
+
+			players.Add(newPlayer);
+			return true;
+		}
 		public bool InitializeGame()
-        {
+		{
 			if (status == 1 || players.Count < 2)
 				return false;
 			status = 1;
@@ -24,55 +34,48 @@ namespace OnecardServer
 			SetDeck();
 
 			SplitCard();
-			
+
 			return true;
 		}
 
-		public HostID[] GetHostIDs()
-        {
-			HostID[] users = players
-				.Select(p => p.user.HostId)
-				.ToArray();
-			return users;
-		}
-		public int GetPlayerID(User user)
-        {
-			GameRoom room = ServerLauncher.RoomArray[user.RoomNumber];
-			return room.players.FindIndex(p => p.user.HostId == user.HostId);
-		}
 		private void SetDeck()
 		{
 			GameCard[] deck = new GameCard[53];
 			Random random = new Random();
 
+			// 배열에 카드를 랜덤으로 생성합니다.
 			for (int i = 0; i < deck.Length - 1;)
 			{
 				int rand = random.Next(0, 52);
 				if (deck[rand] == null) deck[rand] = new GameCard(i++);
 			}
 
+			// usedDeck에 카드를 추가합니다.
 			for (int i = 0; i < deck.Length - 1; i++)
 				usedDeck.Push(deck[i]);
 
+			// usedDeck에 있는 카드를 섞어 unusedDeck으로 옮깁니다.
 			MergeDeck();
 		}
+
 		private void MergeDeck()
 		{
 			GameCard[] deck = new GameCard[53];
 			Random random = new Random();
 
-			while( usedDeck.Count() > 0)
+			while (usedDeck.Count() > 0)
 			{
 				int rand = random.Next(0, 52);
 				if (deck[rand] == null)
 					deck[rand] = usedDeck.Pop();
-            }
-			for(int i=0; i<deck.Length; i++)
-            {
+			}
+			for (int i = 0; i < deck.Length; i++)
+			{
 				if (deck[i] == null) continue;
 				unusedDeck.Push(deck[i]);
-            }
+			}
 		}
+
 		private void SplitCard()
 		{
 			for (int j = 0; j < 7; j++)
@@ -82,28 +85,25 @@ namespace OnecardServer
 			usedDeck.Push(unusedDeck.Pop());
 			lastCard = usedDeck.Peek();
 		}
-
-		public GameCard getLastCard()
+		public void EndGame()
 		{
-			return usedDeck.Peek();
+			status = 0;
 		}
+
 		public void setNextTurn()
 		{
 			if (++turn == players.Count())
 				turn = 0;
 		}
-		public void EndGame()
-        {
-			status = 0;
-        }
 		public int DrawHand()
-        {
+		{
 			GameCard gameCard = unusedDeck.Pop();
 			players[turn].hand.Add(gameCard);
 			return 1;
 		}
-        public bool PlayHand(int hand)
+		public bool PlayHand(int hand)
 		{
+			// 핸드 개수 확인
 			int currentHand = players[turn].hand.Count();
 			if (hand < 0 || hand >= currentHand)
 				return false;
@@ -120,15 +120,23 @@ namespace OnecardServer
 			}
 			return false;
 		}
-		public bool EnterPlayer(User user)
+
+		public HostID[] GetHostIDs()
         {
-			int numPlayer = players.Count();
-			if (numPlayer >= 4) return false;
+			HostID[] users = players
+				.Select(p => p.user.HostId)
+				.ToArray();
+			return users;
+		}
+		public int GetPlayerID(User user)
+        {
+			GameRoom room = ServerLauncher.RoomArray[user.RoomNumber];
+			return room.players.FindIndex(p => p.user.HostId == user.HostId);
+		}
 
-			GamePlayer newPlayer = new GamePlayer(user);
-
-			players.Add(newPlayer);
-			return true;
+		public GameCard getLastCard()
+		{
+			return usedDeck.Peek();
 		}
 
 	}
--- ProudnetOnecard_rewrite/OnecardServer/process/GameRoomProcess.cs
+++ ProudnetOnecard_rewrite/OnecardServer/process/GameRoomProcess.cs
@@ -24,10 +24,9 @@ namespace OnecardServer.process
             if (!isSuccess)
                 return true;
 
-            S2CProxy.NotifyStartGame(room.GetHostIDs(), RmiContext.ReliableSend, 0, room.getLastCard());
-            for(int i = 0; i < room.players.Count; i++)
+            S2CProxy.NotifyStartGame(room.GetHostIDs(), RmiContext.ReliableSend, 0, room.lastCard);
+            for (int i = 0; i < room.players.Count; i++)
             {
-                Console.WriteLine("Handling... {0}", i);
                 GamePlayer player = room.players[i];
                 S2CProxy.ResponseDraw(player.user.HostId, rmiContext, player.hand);
             }
@@ -43,29 +42,31 @@ namespace OnecardServer.process
             GameRoom room = ServerLauncher.RoomArray[RoomNumber];
             int playerID = room.GetPlayerID(user);
 
-            if (playerID == room.turn)
+            if (playerID != room.turn)
+                return true;
+            
+            GamePlayer player = room.players[playerID];
+            int countHand = player.hand.Count;
+            int hand = player.hand.FindIndex(p => p.toNumber() == card.toNumber());
+            bool isSuccess = room.PlayHand(hand);
+            if (!isSuccess)
+                return true;
+
+            var hosts = room.GetHostIDs();
+            S2CProxy.ChangeLastCard(hosts, rmiContext, room.getLastCard());
+            S2CProxy.ChangeHand(hosts, rmiContext, playerID, player.hand.Count());
+
+            if (countHand == 1)
             {
-                GamePlayer player = room.players[playerID];
-                int countHand = player.hand.Count;
-                int hand = player.hand.FindIndex(p => p.toNumber() == card.toNumber());
-                bool isSuccess = room.PlayHand(hand);
-                if (isSuccess)
-                {
-                    var hosts = room.GetHostIDs();
-                    S2CProxy.ChangeLastCard(hosts, rmiContext, room.getLastCard());
-                    S2CProxy.ChangeHand(hosts, rmiContext, playerID, player.hand.Count());
-                    if (countHand == 1)
-                    {
-                        room.EndGame();
-                        S2CProxy.NotifyEndGame(hosts, rmiContext, playerID);
-                    }
-                    else
-                    {
-                        room.setNextTurn();
-                        S2CProxy.ChangeTurn(hosts, rmiContext, room.turn);
-                    }
-                }
+                room.EndGame();
+                S2CProxy.NotifyEndGame(hosts, rmiContext, playerID);
             }
+            else
+            {
+                room.setNextTurn();
+                S2CProxy.ChangeTurn(hosts, rmiContext, room.turn);
+            }
+            
             return true;
         }
         static public bool DrawCard(HostID remote, RmiContext rmiContext)
@@ -78,20 +79,19 @@ namespace OnecardServer.process
             GameRoom room = ServerLauncher.RoomArray[RoomNumber];
             int playerID = room.GetPlayerID(user);
 
-            if (playerID == room.turn)
+            if (playerID != room.turn)
+                return true;
+
+            GamePlayer player = room.players[playerID];
+            int count = room.DrawHand();
+            if (count > 0)
             {
-                GamePlayer player = room.players[playerID];
-                int count = room.DrawHand();
-                if (count > 0)
-                {
-                    var hosts = room.GetHostIDs();
-                    S2CProxy.ChangeHand(hosts, rmiContext, playerID, player.hand.Count());
-                    S2CProxy.ResponseDraw(player.user.HostId, rmiContext, player.hand);
-                    room.setNextTurn();
-                    S2CProxy.ChangeTurn(hosts, rmiContext, room.turn);
-                }
+                var hosts = room.GetHostIDs();
+                S2CProxy.ChangeHand(hosts, rmiContext, playerID, player.hand.Count());
+                S2CProxy.ResponseDraw(player.user.HostId, rmiContext, player.hand);
+                room.setNextTurn();
+                S2CProxy.ChangeTurn(hosts, rmiContext, room.turn);
             }
-            return true;
 
             return true;
         }
